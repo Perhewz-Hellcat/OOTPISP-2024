50171c9c466c3b7ac3a6377212a257383e3e0098
perhewz <149831064+Perhewz-Hellcat@users.noreply.github.com>
2024-05-29 21:29:35 +0300

    Update Pair.h

---
+++ trunk/po0_220216/task_05/src/Pair.h
@@ -0,0 +1,54 @@
+#pragma once
+#ifndef PAIR_H
+#define PAIR_H
+#include <iostream>
+
+class Pair
+{
+public:
+    Pair(float f = 0, float s = 0) : first(f), second(s) {}
+    Pair(const Pair &p) = default;
+    ~Pair() = default;
+
+    Pair &operator=(const Pair &p) = default;
+    bool operator==(const Pair &p) const
+    {
+        return (first == p.first) && (second == p.second);
+    }
+
+    bool operator<(const Pair &p) const
+    {
+        return (first < p.first) || (first == p.first && second < p.second);
+    }
+
+    bool operator>(const Pair &p) const
+    {
+        return (first > p.first) || (first == p.first && second > p.second);
+    }
+
+    Pair operator+(const Pair &p) const
+    {
+        return Pair(first + p.first, second + p.second);
+    }
+
+    friend std::ostream &operator<<(std::ostream &out, const Pair &p)
+    {
+        out << p.first << ", " << p.second;
+        return out;
+    }
+
+    friend std::istream &operator>>(std::istream &in, Pair &p)
+    {
+        std::cout << "Enter first element: ";
+        in >> p.first;
+        std::cout << "Enter second element: ";
+        in >> p.second;
+        return in;
+    }
+
+private:
+    float first;
+    float second;
+};
+
+#endif
